4fecf63ccd2dd6d505ab2e9df2cf96dd0dc55012
tranphuc7686@gmail.com <Tran Phuc>
2019-10-18 00:22:22 +0700

    update code

---
+++ TCPProject/ViewModel/ApplicationAdminViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TCPProject.Model;
+
+namespace TCPProject.ViewModel
+{
+    public class ApplicationAdminViewModel
+    {
+        public Applications applications { get; set; }
+        public int CountElementPending { get; set; }
+    }
+}
